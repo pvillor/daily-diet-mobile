fc5d754ba3c3d769c701db5b77f65618f61a8f2e
paulo.guerra <paulo.guerra@flex-am.com.br>
2024-12-05 10:44:40 -0400

    config: style guide and path mapping

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Text } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import { NunitoSans_400Regular, NunitoSans_700Bold, useFonts } from '@expo-google-fonts/nunito-sans'
 
-export default function App() {
+import theme from '@theme/index';
+
+export function App() {
+  const [isFontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      {!isFontsLoaded ? <Text>Link Start</Text> : <ActivityIndicator />}
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
