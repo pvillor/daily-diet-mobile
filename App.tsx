efa43f8f2214fa9e569a9f520f60edc042dd84a5
paulo.guerra <paulo.guerra@flex-am.com.br>
2024-12-05 11:04:51 -0400

    feat: loading component

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
 import { ActivityIndicator, Text } from 'react-native';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components/native';
 import { NunitoSans_400Regular, NunitoSans_700Bold, useFonts } from '@expo-google-fonts/nunito-sans'
 
 import theme from '@theme/index';
+import { Loading } from '@components/loading';
 
 export function App() {
   const [isFontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold })
 
   return (
     <ThemeProvider theme={theme}>
-      {!isFontsLoaded ? <Text>Link Start</Text> : <ActivityIndicator />}
+      {!isFontsLoaded ? <Text>Link Start</Text> : <Loading />}
     </ThemeProvider>
   );
 }
